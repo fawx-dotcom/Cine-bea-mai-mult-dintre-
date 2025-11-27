cf861df8334d6be5758c79969f88884e167cd16c
Raresh Andrei <raresandreiro11@icloud.com>
2025-11-27 23:26:09 +0200

    Update initial candidates in CandidateService

--- src/services/CandidateService.js
+++ src/services/CandidateService.js
@@ -1,12 +1,26 @@
 const STORAGE_KEY = 'cine_bea_mai_mult_data';
 
 const INITIAL_CANDIDATES = [
-  { id: '1', name: 'Andrei', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Andrei', votes: 12 },
-  { id: '2', name: 'Maria', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Maria', votes: 15 },
-  { id: '3', name: 'Ionut', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Ionut', votes: 8 },
-  { id: '4', name: 'Elena', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Elena', votes: 20 },
-  { id: '5', name: 'Radu', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Radu', votes: 5 },
-  { id: '6', name: 'Ana', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Ana', votes: 18 },
+  { id: '1', name: 'Tavisimo', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Tavisimo', votes: 0 },
+  { id: '2', name: 'Aris', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Aris', votes: 0 },
+  { id: '3', name: 'Rares', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Rares', votes: 0 },
+  { id: '4', name: 'Matei', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Matei', votes: 0 },
+  { id: '5', name: 'Valentina (val)', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Valentina', votes: 0 },
+  { id: '6', name: 'Alexandra', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Alexandra', votes: 0 },
+  { id: '7', name: 'Melina', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Melina', votes: 0 },
+  { id: '8', name: 'Tudor melu', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Tudor', votes: 0 },
+  { id: '9', name: 'Bianca preda', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Bianca', votes: 0 },
+  { id: '10', name: 'dorina', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=dorina', votes: 0 },
+  { id: '11', name: 'mihnea', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=mihnea', votes: 0 },
+  { id: '12', name: 'Maria Lavinia', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Maria', votes: 0 },
+  { id: '13', name: 'Laur', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Laur', votes: 0 },
+  { id: '14', name: 'Alexia', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Alexia', votes: 0 },
+  { id: '15', name: 'Andrei Stanciu', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Andrei', votes: 0 },
+  { id: '16', name: 'Vlad', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Vlad', votes: 0 },
+  { id: '17', name: 'Bella', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Bella', votes: 0 },
+  { id: '18', name: 'Tony', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Tony', votes: 0 },
+  { id: '19', name: 'Teo', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Teo', votes: 0 },
+  { id: '20', name: 'Robert', image: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Robert', votes: 0 },
 ];
 
 export const CandidateService = {
@@ -66,3 +80,4 @@ export const CandidateService = {
       return candidates.reduce((prev, current) => (prev.votes > current.votes) ? prev : current);
   }
 };
+
